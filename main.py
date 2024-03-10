b0188603776a89af0178f6e8e65b9567892d7727
12nikki21 <veronikabojko543@gmail.com>
2024-03-10 17:58:45 +0200

    student artemka

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+from random import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Student:
+
+ def __init__(self, name):
+       self.name = name
+
+       #Happy
+       self.gladness = 50
+
+       #progress
+       self.progress = 0
+
+       #Alien
+       self.alive = True
+
+ def to_study(self):
+    print("Time to study! :((((")
+    self.progress += 0.12
+    self.gladness -= 3
+
+ def to_sleep(self):
+    print("I will sleep.")
+    self.gladness += 3
+ def to_chill(self):
+    print("rest time")
+    self.gladness += 5
+    self.progress -= 0.1
+
+ def is_alive(self):
+    if self.progress < -0.5:
+        print("You are a corp.")
+        self.alive = False
+
+    if self.gladness < 0:
+        print("You are a suicide lover. ;)")
+        self.alive = False
+
+    if self.progress > 5:
+        print("Guy, you are cool. You are not as dumb as I thought.")
+        self.alive = False
+
+ def end_of_day(self):
+    print(f"Gladness = {self.gladness}")
+    print(f"Progress = {self.progress}")
+    print()
+
+ def live(self, day):
+    print(f"Day - {day} of {self.name}")
+
+    live_cube = randint(1,3)
+
+    if live_cube == 1:
+        self.to_study()
+
+    elif live_cube == 2:
+        self.to_chill()
+
+    elif live_cube == 3:
+        self.to_sleep()
+
+    self.end_of_day()
+
+    self.is_alive()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+Artem = Student("Artem Sigma")
+for day in range(365):
+    if Artem.alive == False:
+        break
+
+    Artem.live(day)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
